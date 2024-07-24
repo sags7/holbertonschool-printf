2f2ca18f1724d41015345ba246b0a26207d1e977
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-24 13:10:22 -0500

    manually printing a null character \0 at the end

--- _printf.c
+++ _printf.c
@@ -33,7 +33,7 @@ int _printf(const char *format, ...)
 		switch (format[i])
 		{
 		/**
-		*case '\\':	 unimplemented
+		*case '\\':	 unimplemented 
 		*	i++;
 		*	break;
 		*/
@@ -50,7 +50,9 @@ int _printf(const char *format, ...)
 	i = 0;
 	while (finishedStr[i])
 		_putchar(finishedStr[i++]);
-
+	
+	if (!finishedStr[i]) 
+		_putchar('\0');
 
 	va_end(arguments);
 	free(finishedStr);

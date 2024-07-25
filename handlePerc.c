bb28fef45334ed2e87b361d134c22d1a6d5bc584
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-24 19:14:34 -0500

    handled a format string with just a %

--- handlePerc.c
+++ handlePerc.c
@@ -13,13 +13,17 @@ void handlePerc(workingData *wd)
 		{'\0', NULL}};
 	int i = 0;
 
-
-	if (wd->inputStr[*wd->inputPosition] == '%'
-		&& wd->inputStr[*wd->inputPosition + 1] == '%')
+	if (wd->inputStr[*wd->inputPosition] == '%') 
 	{
-		handleEscPerc(wd);
-		return;
+		if (wd->inputStr[*wd->inputPosition + 1] == '%')
+		{
+			handleEscPerc(wd);
+			return;
+		}
+		else if (!(wd->inputStr[*wd->inputPosition + 1]))
+			return;
 	}
+	
 	while (wd->inputStr[*wd->inputPosition])
 	{
 		i = 0;

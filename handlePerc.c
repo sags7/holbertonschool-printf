db7b04d66152e0d751306f32d5e831cc51dbe3ba
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-07-25 11:33:19 -0500

    created getHandler() helper function for clarity

--- handlePerc.c
+++ handlePerc.c
@@ -1,9 +1,6 @@
 #include "main.h"
-/**
- * handlePerc - handles formatted inputs with %
- * @wd: the struct with the necessary data to format string
- */
-int handlePerc(workingData *wd)
+/* int (*)(workingData *wd) */
+int (*getHandler(workingData *wd))(workingData *wd)
 {
 	format dtypes[] = {
 		{'s', handleStr},
@@ -13,15 +10,13 @@ int handlePerc(workingData *wd)
 		{'\0', NULL}};
 	int i = 0;
 
+	
 	if (wd->inputStr[*wd->inputPosition] == '%') 
 	{
 		if (wd->inputStr[*wd->inputPosition + 1] == '%')
-		{
-			handleEscPerc(wd);
-			return (0);
-		}
+			return(&handleEscPerc);
 		else if (!(wd->inputStr[*wd->inputPosition + 1]))
-			return (0);
+			return (NULL);
 	}
 	
 	while (wd->inputStr[*wd->inputPosition])
@@ -32,13 +27,27 @@ int handlePerc(workingData *wd)
 			if (wd->inputStr[*wd->inputPosition] == dtypes[i].format
 				&& dtypes[i].format != '\0')
 			{
-				dtypes[i].handler(wd);
-				return (0);
+				return(dtypes[i].handler);
 			}
 			i++;
 		}
 		(*wd->inputPosition)++;
 	}
+	return (NULL);
+}
+
+/**
+ * handlePerc - handles formatted inputs with %
+ * @wd: the struct with the necessary data to format string
+ * Return: 0 if successful or -n if not
+ */
+int handlePerc(workingData *wd)
+{
+	int (*handler)(workingData *wd) = getHandler(wd);
+	
+	if (handler(wd) < 0 )
+		return(-1);
+		
 	return (0);
 	/*pending logic for when there is no match for %x*/
 }
